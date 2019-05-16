034dfb2e5e6053c71879d840a047b9f519fb6ac0
stayl <staylimmk@gmail.com>
2019-05-16 22:52:04 +0500

    Добавьте файлы проекта.

---
+++ task_three/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task_three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите х:   ");
+            double x = EnterANumber();
+            Console.Write("Введите у:   ");
+            double y = EnterANumber();
+
+            if (IsBelongTo(x, y)) Console.WriteLine($"Точка [{x}; {y}] принадлежит единичной окружности.");
+            else Console.WriteLine($"Точка [{x}; {y}] не принадлежит единичной окружности.");
+
+            Console.ReadLine();
+        }
+
+        static double EnterANumber()
+        {
+            bool ok = false;
+            double number = 0;
+            do
+            {
+                try
+                {
+                    number = Convert.ToDouble(Console.ReadLine());
+                    ok = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Введите действительное число!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Введите действительное число!");
+                }
+            } while (!ok);
+            return number;
+        }
+
+        static bool IsBelongTo(double x, double y)
+        {
+            return Math.Pow(x, 2) + Math.Pow(y, 2) <= 1;
+        }
+    }
+}
